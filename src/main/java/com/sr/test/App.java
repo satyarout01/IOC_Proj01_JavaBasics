473d705396694520558f8bd301f30537df653849
SATYA <SATYA@DESKTOP-4F4CFAG>
2019-10-17 09:36:20 +0530

    SR Commit 01

---
+++ src/main/java/com/sr/test/App.java
@@ -0,0 +1,14 @@
+package com.sr.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("App.main()");
+    }
+}
